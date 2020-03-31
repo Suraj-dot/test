be19784c60ce7d8899c027f56e43c33b8fca5341
Suraj-dot <62868667+Suraj-dot@users.noreply.github.com>
2020-03-31 12:13:54 +0530

    Add files via upload

---
+++ BusinessObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Erp;
+using Ice;
+
+namespace Stcl.CIFMIS.WarrantReleaseEntry
+{
+    public class BusinessObject
+    {
+
+        public decimal OrigDebitAmount { get; set; }
+        public decimal OrigCreditAmount { get; set; }
+        public string OrigGLAccount { get; set; }
+        public decimal AvailBudget { get; set; }
+        public string Vote { get; set; }
+        public string VoteCompany { get; set; }
+        public string GlAcctDisp { get; set; }
+        public Int32 MaxNo { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Prefix { get; set; }
+    }
+}
